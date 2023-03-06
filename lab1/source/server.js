b8a3931b4cebe38100e811905c65e2b95b0fe1a9
Tobiasz Kostrzewa <tobiasz.kostrzewa@wp.pl>
2023-03-06 15:34:10 +0100

    Add query params handling

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let videoQuery = req.query.videoFile;
+    let audioQuery = req.query.audioFile;
+    res.send(`<video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
+    	<audio id="audioPlayer" controls src=${audioQuery}></audio>`);
 })
 
 app.listen(4080)
