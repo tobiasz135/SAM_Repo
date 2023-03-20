c90e2a5144ca7431a227662848586bb4bad467a9
Tobiasz <tobiasz.kostrzewa@wp.pl>
2023-03-20 14:35:07 +0100

    Add image handling from query

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -5,8 +5,14 @@ const app = express()
 app.get('/', (req, res) => {
     let videoQuery = req.query.videoFile;
     let audioQuery = req.query.audioFile;
-    res.send(`<video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
-    	<audio id="audioPlayer" controls src=${audioQuery}></audio>`);
+    let imgQuery = req.query.imgFile;
+    let imgString = ` <img id="posterImage" src="${imgQuery}" /> `;
+    let outputString = `<video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
+    <audio id="audioPlayer" controls src=${audioQuery}></audio> `
+    if(imgQuery){
+        outputString += imgString;
+    }
+    res.send(outputString);
 })
 
 app.listen(4080)
